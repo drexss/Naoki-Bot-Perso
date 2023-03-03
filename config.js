ab518bb9f443576eae0fec6c8739e78a5ed1b903
drexss <126890935+drexss@users.noreply.github.com>
2023-03-03 15:48:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     app: {
         px: '+',
-        token: '',
-        owners: '843204602686078976',
+        token: 'MTA4MTIyNDczNjM1MDE2MzA0NA.Gfb6Ct.38dpXB0q91IfDM9lMcApCTjmICHRlbdRM52cv0',
+        owners: '1081215845176455198',
         funny: '843204602686078976',
         color: '#FF0000',
         footer: 'sltcv ?',
